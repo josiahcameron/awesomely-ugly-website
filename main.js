40d2a34aff806f2b2c46d9dfe0a64832d3753bc0
josiahcameron <josiahcameron2015@gmail.com>
2023-02-24 18:39:45 -0500

    Added HTML Elements and Image

---
+++ main.js
@@ -0,0 +1,8 @@
+const myImage = document.querySelector("#myImage");
+
+myImage.addEventListener("click", () => {
+	myImage.setAttribute(
+		"src",
+		"https://i.kym-cdn.com/photos/images/facebook/001/847/137/ec5"
+	);
+});
